44aac497e8feeaa28712aad251a738724b622595
Megan Day <meganeday7@gmail.com>
2021-04-28 19:52:08 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-
+const superbowlWin = (array) => {
+    const yearWon = array.find(obj => {return obj.result === "W"})
+    if (yearWon != undefined) {
+        return yearWon.year
+    } else {return undefined}
+}
