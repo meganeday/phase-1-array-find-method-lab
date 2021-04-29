1ec689710864776f66190abde8f51f1b4385ca79
Megan Day <meganeday7@gmail.com>
2021-04-28 19:51:28 -0500

    commit tree 55e6cbb0fccfcb496d1bc55b000086d1fd24213f

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const superbowlWin = (array) => {
+    const yearWon = array.find(obj => {return obj.result === "W"})
+    if (yearWon != undefined) {
+        return yearWon.year
+    } else {return undefined}
+}
 
+// function superbowlWin (array) {
+//     const win = array.find(obj => {return obj.result === 'W'})
+//     if (win != undefined) {
+//         return win.year
+//         } else {
+//             return undefined
+//     }
+// }
